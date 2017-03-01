Fix bug in computation of unbiased variance.
Performance still not good :( .

assignment-1/ladder/ladder-ak/ladder.py
@@ -169,7 +169,9 @@ class Encoder(torch.nn.Module):
             var = np.var(x.data.numpy(), axis=0).reshape(1, x.size()[1])
             var = Variable(torch.FloatTensor(var))
             # unbiased_var = var * (float(x.size()[0]) / (float(x.size()[0]) - 1.))
-            unbiased_var = np.var(x.data.numpy(), ddof=1, axis=0).reshape(1, x.size()[1])
+            # unbiased_var = np.var(x.data.numpy(), ddof=1, axis=0).reshape(1, x.size()[1])
+            # unbiased_var = Variable(torch.FloatTensor(unbiased_var))
+            unbiased_var = torch.var(x, 0)
             # Updating values for validation/test phase
             self.running_mean = ((1.0 - momentum) * self.running_mean) + momentum * mean
             self.running_var = ((1.0 - momentum) * self.running_var) + momentum * unbiased_var
@@ -209,15 +211,16 @@ class Encoder(torch.nn.Module):
         # TODO: Check whether you have to detach this or not.
         self.buffer_z_pre = z_pre.detach().clone()
         z_pre_norm = self.bn_normalize(z_pre)
-        z_pre_norm_test = self.bn_test(z_pre)
+        # z_pre_norm_test = self.bn_test(z_pre)
         # z_pre_norm = z_pre_norm_test
-        if not self.training and epoch_global == 12:
-            print("#" * 50)
-            print(z_pre_norm)
-            print("-" * 50)
-            print(z_pre_norm_test)
-            print("#" * 50)
-            _ = raw_input("press key to continue: ")
+        # if not self.training and epoch_global == 5:
+        #     print(np.all(np.isclose(z_pre_norm.data, z_pre_norm_test.data)))
+            # print("#" * 50)
+            # print(z_pre_norm)
+            # print("-" * 50)
+            # print(z_pre_norm_test)
+            # print("#" * 50)
+            # _ = raw_input("press key to continue: ")
         # Add noise
         noise = np.random.normal(loc=0.0, scale=self.noise_level, size=z_pre_norm.size())
         noise = Variable(torch.FloatTensor(noise))