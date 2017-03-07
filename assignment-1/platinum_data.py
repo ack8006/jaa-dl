e55297266b1237c9aac710f187435c32b36a0f39
darkstar112358 <abhishekkadiyan@gmail.com>
2017-03-06 23:02:02 -0500

    Add the fist ensemble.

--- assignment-1/platinum_data.py
+++ assignment-1/platinum_data.py
@@ -1,33 +1,40 @@
 import torch
 import pickle
 import numpy as np
-# from convnet_leaky_relu import ConvNet
-# from convnet_heavy_dropout import ConvNet
-# from convnet3_deep3 import ConvNet
-from convnet3_deep2 import ConvNet
 from torch.autograd import Variable
 
+# from convnet3_deep3 import ConvNet
+# model_name = "best_cnn_d12d25b16e118acc9932.model"
 
-# model = torch.load("saved_models_leaky_relu/best_cnn_d14d25b32e766acc9929.model")
-# model = torch.load("saved_models3/best_heavy_dropout_d2525252555b16e195acc9925.model")
-# model = torch.load("saved_models3/best_cnn_d12d25b16e118acc9932.model")
-model = torch.load("saved_models2/best_cnn_d4b16e73acc9929.model")
+# from convnet_heavy_dropout import ConvNet
+# model_name = "best_heavy_dropout_d2525252555b16e195acc9925.model"
+
+from convnet_leaky_relu import ConvNet
+model_name = "best_cnn_d14d25b32e766acc9929.model" # leaky-relu
+
+model = torch.load("ensemble-models/" + model_name)
 
 model.eval()
-test_old = pickle.load(open("data/test_old.p"))
-test_data = test_old.test_data.numpy()
-test_data = np.array([(x.flatten()/255.0).reshape(1,28,28) for x in test_data])
-test_data = torch.from_numpy(test_data).float().resize_(len(test_data),1,28,28)
-test_labels = test_old.test_labels
+
+valid_data = pickle.load(open("data/valid_data.p"))
+valid_data = np.array([(x.flatten()/255.0).reshape(1,28,28) for x in valid_data])
+valid_data = torch.from_numpy(valid_data).float().resize_(len(valid_data),1,28,28)
+
+# test_old = pickle.load(open("data/test_old.p"))
+# test_data = test_old.test_data.numpy()
+# test_data = np.array([(x.flatten()/255.0).reshape(1,28,28) for x in test_data])
+# test_data = torch.from_numpy(test_data).float().resize_(len(test_data),1,28,28)
+# test_labels = test_old.test_labels
 
 def predict(m, x_val):
     x = Variable(x_val, requires_grad=False)
     output = m.forward(x)
     return output.data.numpy()
 
-pred_y = predict(model, test_data)
+pred_y = predict(model, valid_data)
+# pred_y = predict(model, test_data)
 
-with open("preds_conv3_deep2_sun_best_test_old.p", "wb") as f:
+with open("ensemble_predictions" + model_name + ".p", "wb") as f:
     pickle.dump(pred_y, f)
 
 
